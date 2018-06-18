f8691484738b9b7902c9b2a7c6ebbde02981f35d
DianaOlvera <DianaOlvera@users.noreply.github.com>
2018-06-18 08:47:18 -0500

    completo con css

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 buttonStart.addEventListener('click', () =>{
 
@@ -7,8 +6,6 @@ buttonStart.addEventListener('click', () =>{
 });
 
 
-=======
->>>>>>> a7c3e3083ee8a88f95f1afb789073b948136b45b
 let string=document.getElementById("entrada");// llamo frase de usuario
 let offset=document.getElementById("offset");// llamo los saltos
 let buttonEncode=document.getElementById("button");
@@ -18,23 +15,14 @@ let buttonDecode=document.getElementById("button2");
 
 
 
-<<<<<<< HEAD
 
 
 buttonEncode.addEventListener("click", () => {
 document.getElementById("resultado").innerHTML = window.cipher.encode( parseInt(offset.value), string.value);
-=======
-buttonEncode.addEventListener("click", () => {
-document.getElementById("cifrado").innerHTML = window.cipher.encode( string.value, parseInt(offset.value));
->>>>>>> a7c3e3083ee8a88f95f1afb789073b948136b45b
 }
 ),
 
 buttonDecode.addEventListener("click", () => {
-<<<<<<< HEAD
   document.getElementById("resultado").innerHTML = window.cipher.decode( parseInt(offset.value), string2.value);
-=======
-  document.getElementById("descifrado").innerHTML = window.cipher.decode( string2.value, parseInt(offset.value));
->>>>>>> a7c3e3083ee8a88f95f1afb789073b948136b45b
 }
 )
